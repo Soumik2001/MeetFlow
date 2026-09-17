6eeb0b90069d54943c387d77787f067fb104bb0a
Soumik2001 <soumikmondal61116@gmail.com>
2026-09-17 23:28:27 +0530

    Header Footer Changes Done

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <footer className='w-full max-w-305 mx-auto bg-white/10 backdrop-blur xl:rounded-t-xl px-6 py-4 text-center font-medium'>
+            <p>&copy; {new Date().getFullYear()} Meetup. All right reserved.</p>
+        </footer>
     )
 }
 
